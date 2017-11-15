59721df98c467a611d8647bea6809a4e6c030c57
Tobias Hedkvist <tobias.hedkvist@hotmail.com>
2017-11-15 09:58:08 +0100

    second commit, added functional coed fr both classes

--- week8-exercise01/src/main/java/com/exercises/maven/week8_exercise01/Main.java
+++ week8-exercise01/src/main/java/com/exercises/maven/week8_exercise01/Main.java
@@ -3,7 +3,15 @@ package com.exercises.maven.week8_exercise01;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MyRandomMethods mrm = new MyRandomMethods();
+		
+		System.out.println(mrm.randomFrom1To10());
+		System.out.println(mrm.randomFrom11To20());
+		System.out.println(mrm.randomFrom21To30());
+		System.out.println(mrm.randomFrom31To40());
+		System.out.println(mrm.randomFrom41To50());
+		
+		
 
 	}
 
--- week8-exercise01/src/main/java/com/exercises/maven/week8_exercise01/MyRandomMethods.java
+++ week8-exercise01/src/main/java/com/exercises/maven/week8_exercise01/MyRandomMethods.java
@@ -1,5 +1,33 @@
 package com.exercises.maven.week8_exercise01;
 
-public class MyRandomMethods {
+import java.util.Random;
 
+public class MyRandomMethods {
+	Random random = new Random();
+	
+	public int randomFrom1To10() {
+		int result = 0;
+		result = random.nextInt(10)+1;
+		return result;
+	}
+	public int randomFrom11To20() {
+		int result = 0;
+		result = random.nextInt(10)+11;
+		return result;
+	}
+	public int randomFrom21To30() {
+		int result = 0;
+		result = random.nextInt(10)+21;
+		return result;
+	}
+	public int randomFrom31To40() {
+		int result = 0;
+		result = random.nextInt(10)+31;
+		return result;
+	}
+	public int randomFrom41To50() {
+		int result = 0;
+		result = random.nextInt(10)+41;
+		return result;
+	}
 }
